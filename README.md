2cd8b7e260af97a2de521ab04ef73fac5121aac8
Luxaubon <>
2023-10-09 20:29:37 +0700

    first commit

--- README.md
+++ README.md
@@ -34,3 +34,23 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+```bash
+npm install react-responsive-carousel
+
+npm install axios cheerio
+
+npm install mongoose
+
+npm install @headlessui/react
+
+npm install nodemailer
+
+npm install --save-dev @types/nodemailer
+
+npm install supports-color@8.1.1
+
+https://brightdata.com/
+
+https://account.mongodb.com/
+```
---
+++ components/Searchbar.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { FormEvent, useState } from "react"
+import {scrapeAndStoreProduct} from '@/lib/actions';
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    if(
+      hostname.includes('amazon.com') || 
+      hostname.includes ('amazon.') || 
+      hostname.endsWith('amazon')
+    ) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+
+  return false;
+}
+
+function Searchbar() {
+    const [isLoading, setIsLoading] = useState(false)
+    const [searchPrompt, setSearchPrompt] = useState('')
+    
+    const handleSubmit = async (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const isValidLink = isValidAmazonProductURL(searchPrompt);
+
+        if(!isValidLink) return alert('Please provide a valid Amazon link')
+    
+        try {
+          setIsLoading(true);
+    
+          // Scrape the product page
+          const product = await scrapeAndStoreProduct(searchPrompt);
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setIsLoading(false);
+        }
+
+    }
+  return (
+    <form 
+      className="flex flex-wrap gap-4 mt-12" 
+      onSubmit={handleSubmit}
+    >
+      <input 
+        type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
+        placeholder="Enter product link"
+        className="searchbar-input"
+      />
+
+      <button 
+        type="submit" 
+        className="searchbar-btn"
+        disabled={searchPrompt === ''}
+      >
+        {isLoading ? 'Searching...' : 'Search'}
+      </button>
+    </form>
+  )
+}
+
+export default Searchbar
